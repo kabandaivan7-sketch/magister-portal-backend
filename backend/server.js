6a55a5e3391ebc28838818972f906dcba8088bc6
kabandaivan7-sketch <kabandaivan7@gmail.com>
2026-01-02 06:25:51 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,14 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 // Security & Middleware
 // ===============================
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "https://magisterportal.netlify.app"
+  ]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(compression());
